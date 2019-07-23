16d2db8128bd2224788ca7557a27b2651d244359
sslmyo24 <sslmyo24@gmail.com>
2019-07-23 20:47:56 +0900

    Create solution.js

---
+++ 해시/베스트앨범/solution.js
@@ -0,0 +1,19 @@
+function solution(genres, plays) {
+    let saleSum = {}, result = [];
+    plays.forEach((v, k) => saleSum[genres[k]] = saleSum[genres[k]] ? saleSum[genres[k]] + v : v);
+    const sortSum = objSort(saleSum);
+    for (const saleData of sortSum) {
+        let saleArr = {};
+        genres.forEach((v, k) => { if (v === saleData[0]) saleArr[k] = plays[k] });
+        const sortSale = objSort(saleArr);
+        for (let i = 0, len = sortSale.length; i < len && i < 2; i++) result.push(~~sortSale[i][0]);
+    }
+    return result;
+}
+
+function objSort (obj) {
+    let sortable = [];
+    for (const key in obj) sortable.push([key, obj[key]]);
+    sortable.sort((a, b) => b[1] > a[1] ? 1 : b[1] == a[1] ? ~~a[0] > ~~b[0] ? 1 : -1 : -1);
+    return sortable;
+}
