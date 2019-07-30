da7ff773fa60c4c6b66b491952f6ae9862a69df3
sslmyo24 <sslmyo24@gmail.com>
2019-07-31 08:05:12 +0900

    Update Solution.java

--- 힙(Heap)/더 맵게/Solution.java
+++ 힙(Heap)/더 맵게/Solution.java
@@ -1,28 +1,47 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Collections;
-
 class Solution {
     public int solution(int[] scoville, int K) {
-        List<Integer> list = new ArrayList<Integer>();
-        for (int num:scoville) list.add(num);
-        Collections.sort(list);
-        int sum = 0, cnt = 0;
-        while (sum < K && list.size() > 1) {
-            cnt++;
-            sum = list.get(0) + (list.get(1) * 2);
-            list.add(sum);
-            list.remove(0);
-            list.remove(0);
-            for (int i = list.size() - 1; i > 0; i--) {
-                if (list.get(i) < list.get(i - 1)) {
-                    int tmp = list.get(i);
-                    list.set(i, list.get(i - 1));
-                    list.set(i - 1, tmp);
-                } else break;
+        int[] s = new int[scoville.length];
+        int last = 0, count = 0;
+        for (int v : scoville) insert(s, v, last++);
+        if (s.length <= 1) return -1;
+        while (s.length > 1 && s[0] < K) {
+            if (s[0] == -1 || s[1] == -1) return -1;
+            int min = s[1];
+            if (s.length > 2 && s[2] != -1 && min > s[2]) min = s[2];
+            int v = s[0] + (min * 2);
+            delete(s, --last);
+            delete(s, --last);
+            insert(s, v, last++);
+            count += 1;
+        }
+        return count;
+    }
+    
+    static public void insert (int[] s, int v, int last) {
+        int i = last;
+        while ((i > 0) && (v < s[i/2])) {
+            s[i] = s[i/2];
+            i /= 2;
+        }
+        s[i] = v;
+    }
+         
+    static public void delete (int[] s, int last) {
+        int i = 0;
+        s[i] = s[last];
+        while (i <= last) {
+            int v = s[i];
+            int next = i * 2;
+            if (next >= last) break;
+            if (s[next] > s[next + 1]) next += 1;
+            if (v > s[next]) {
+                s[i] = s[next];
+                s[next] = v;
+                i = next;
+            } else {
+                break;
             }
         }
-        if (sum < K) cnt = -1;
-        return cnt;
+        s[last] = -1;
     }
 }
