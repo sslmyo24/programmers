34339305a9b2cd4710e9052b6c099bb22b424e5c
sslmyo24 <sslmyo24@gmail.com>
2019-07-31 21:13:03 +0900

    code to short code

--- 완전탐색/소수 찾기/solution.js
+++ 완전탐색/소수 찾기/solution.js
@@ -21,6 +21,6 @@ function solution(numbers) {
 const boolPrime = number => {
     if (number === 2) return true;
     if (number === 1 || number%2 === 0) return false;
-    for (let i = 3, halfNum = ~~(number/2); i <= halfNum; i += 2) if (number%i === 0) return false;
+    for (let i = 3, sqrt = ~~(Math.sqrt(number)); i <= sqrt; i += 2) if (number%i === 0) return false;
     return true;
 }
--- 완전탐색/카펫/solution.js
+++ 완전탐색/카펫/solution.js
@@ -2,8 +2,7 @@ function solution(brown, red) {
     let divisors = [1];
     for (let i = 2, sqrt = ~~(Math.sqrt(red)); i <= sqrt; i++) if (red%i === 0) divisors.push(i);
     for (const d of divisors) {
-        const ver = (brown - d*2)/2;
-        const hoz = brown/2 - ver + 2;
-        if ((ver - 2) === red/d) return [ver, hoz];
+        const hoz = brown/2 - d
+        if ((hoz - 2) === red/d) return [hoz, d+2];
     }
 }
