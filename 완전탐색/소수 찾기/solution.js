e94c67f8cb348e020382575d8ada84fea13fa3f4
sslmyo24 <sslmyo24@gmail.com>
2019-07-31 21:29:58 +0900

    code to short code

--- 완전탐색/소수 찾기/solution.js
+++ 완전탐색/소수 찾기/solution.js
@@ -2,18 +2,14 @@ function solution(numbers) {
     let answer = 0, num = Math.min(...numbers) > 1 ? Math.min(...numbers) : 2;
     const max = parseInt([...numbers].sort((a, b) => b > a ? 1 : -1).join(''));
     label: for (; num <= max; num++) {
+        if (boolPrime(num) === false) continue label;
         for (let i = 0; i <= 9; i++) if (numbers.indexOf(i) === -1 && num.toString().indexOf(i) !== -1) continue label;
-        let match = false;
         for (const str of numbers) {
-            const reg = new RegExp(str, 'g'), num_match = num.toString().match(reg);
-            if (num_match !== null && numbers.match(reg).length >= num_match.length) match = true;
-            else if (num.toString().indexOf(str) !== -1 && numbers.match(reg).length < num_match.length) {
-                match = false;
-                break;
-            }
+            const reg = new RegExp(str, 'g'), numMatch = num.toString().match(reg);
+            if (numMatch === null) continue;
+            if (numbers.match(reg).length < numMatch.length) continue label;
         }
-        if (match === false) continue label;
-        else if (boolPrime(num) === true) answer++;
+        answer++;
     }
     return answer;
 }
